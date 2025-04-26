077ab15defd05bc8483a6c29018ad8602ba2a845
Simona Anchova <simonaanchova@Simonas-MacBook-Air.local>
2025-04-26 21:19:44 +0200

    Add .gitignore and sign up screen

--- TwitterClone/ContentView.swift
+++ TwitterClone/ContentView.swift
@@ -67,8 +67,38 @@ struct ContentView: View {
                 )
                 .background(Color.white)
             }
+            HStack {
+                Rectangle()
+                    .frame(width: (UIScreen.main.bounds.width / 2) - 65, height: 0.7)
+                Text("or")
+                    .font(.footnote).fontWeight(.light)
+                Rectangle()
+                    .frame(width: (UIScreen.main.bounds.width / 2) - 65, height: 0.7)
+            }.foregroundColor(.gray)
+            Button {
+                print("Create account button")
+            } label: {
+                Text("Create account")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: (UIScreen.main.bounds.width) - 100, height: 50)
+                    .background(Color(UIColor(red: 29/255, green: 161/255, blue: 242/255, alpha: 1.0)))
+                    .cornerRadius(15)
+            }
+            HStack{
+                Text("By signing up, you agree to the ") +
+                Text("Terms of Service ")
+                    .foregroundColor(Color(UIColor(red: 29/255, green: 161/255, blue: 242/255, alpha: 1.0))) +
+                Text("and ") +
+                Text("Privacy Policy, ")
+                    .foregroundColor(Color(UIColor(red: 29/255, green: 161/255, blue: 242/255, alpha: 1.0))) +
+                Text("including ") +
+                Text("Cookie Use.")
+                    .foregroundColor(Color(UIColor(red: 29/255, green: 161/255, blue: 242/255, alpha: 1.0)))
+            }.font(.footnote)
+                .frame(width: (UIScreen.main.bounds.width) - 100, height: 50)
         }
-        .padding()
     }
 }
 
